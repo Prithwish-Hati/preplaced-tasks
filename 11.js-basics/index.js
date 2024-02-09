4d95261a6285f9c490729f356b0554d83bc3861d
Prithwish Hati <hatiprithwish@gmail.com>
2024-02-09 23:42:00 +0530

    js basics

---
+++ 11.js-basics/index.js
@@ -0,0 +1,20 @@
+let hens = 3;
+let roosters = 5;
+
+let totalChickens = hens + roosters;
+console.log("totalChickens= " + totalChickens); //What will be the answer? Explain
+
+hens = hens + 2;
+console.log("Hens= " + hens); //What will be the answer? Explain
+
+console.log("totalChickens= " + totalChickens); //What will be the answer? Explain
+
+totalChickens = hens + roosters;
+console.log("totalChickens= " + totalChickens); //What will be the answer? Explain
+
+// Generating Random numbers between 5 to 10
+let randomNumber = Math.random();
+let int = Math.floor(randomNumber);
+let randomInt = int + 5;
+
+console.log(randomInt);
