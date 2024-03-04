cc22b47ba30decd18d483dcf81281061861ac720
Prithwish Hati <hatiprithwish@gmail.com>
2024-03-04 08:35:15 +0530

    objects

--- 15.currying/script.js
+++ 15.currying/script.js
@@ -8,7 +8,7 @@ function sum(x) {
   };
 }
 
-sum(2)(6)(1);
+// sum(2)(6)(1);
 
 function evaluate(method) {
   return function (x) {
@@ -33,3 +33,48 @@ console.log(evaluate("multiply")(3)(2)); // => 6
 console.log(evaluate("divide")(40)(4)); // => 10
 console.log(evaluate("substract")(4)(4)); // => 0
 console.log(evaluate("substracts")(4)(4)); // => Provide valid method
+
+function add(a) {
+  return function (b) {
+    if (b) return add(a + b);
+    return a;
+  };
+}
+
+console.log(add(5)(2)(4)(8)());
+console.log(add(5)(2)(4)(8)(10)(2)());
+
+function sum(a) {
+  return function (b, c) {
+    return a + b + c;
+  };
+}
+
+const x = sum(10);
+console.log(x(5, 6));
+
+// or
+console.log(sum(20)(1, 4));
+
+function sum1(a) {
+  return function (b) {
+    return function (c) {
+      return a + b + c;
+    };
+  };
+}
+
+const y = sum1(2);
+// console.log(y, 2(5));
+//or
+console.log(sum1(2)(5)(1));
+
+function updateElementText(id) {
+  return function (content) {
+    document.querySelector('#' + id).textContent = content;
+  }
+}
+
+const updateHeader = updateElementText('heading')
+updateHeader("Hello World")
+
---
+++ 16.objects/script.js
@@ -0,0 +1,154 @@
+// let person = {
+//   name: "John",
+//   age: 30,
+//   isStudent: false,
+// };
+
+// // console.log(person.age);
+// // console.log(person["isStudent"]);
+
+// const func = (function (a) {
+//   delete a;
+//   return a;
+// })(5);
+
+// // console.log(5);
+
+// const obj1 = {
+//   channel: "Mr. Beast",
+//   "like the video": true,
+// };
+
+// // console.log(obj1["like the video"]);
+
+// const property = "firstName";
+// const name = "John";
+
+// const user = {
+//   [property]: name,
+// };
+
+// let person1 = {
+//   name: "Jumanji",
+//   age: 30,
+//   isStudent: false,
+// };
+
+// for (key in person1) {
+//   //   console.log(key); // Prints properties
+//   //   console.log(person1[key]); // Prints values
+// }
+
+// const obj = {
+//   a: "one",
+//   b: "two",
+//   a: "three",
+// };
+
+// console.log(obj);
+
+// // Create a function that multiplies all numeric property values by 2.
+// let nums = {
+//   a: 100,
+//   b: 200,
+//   title: "Hello",
+// };
+
+// const a = {};
+// const b = { key: "b" };
+// const c = { key: "c" };
+
+// a[b] = 123; // ->
+// a[c] = 456;
+
+// console.log(a[b]);
+
+// let person2 = {
+//   name: "Jumanji",
+//   age: 30,
+//   isStudent: false,
+// };
+
+// const strObj = JSON.stringify(person2);
+// localStorage.setItem("test", strObj); // If you don't convert it to string it will be [object Object]
+
+// JSON.parse(localStorage.getItem("test")); // Returns the object from localStorage
+
+// console.log([..."Lydia"]);
+
+// const user1 = {
+//   name: "Lydia",
+//   age: 21,
+// };
+
+// const admin = {
+//   admin: true,
+//   ...user1,
+// };
+
+// console.log(admin);
+
+// const settings = {
+//   username: "Lydia",
+//   level: 21,
+//   health: 90,
+// };
+
+// const data = JSON.stringify(settings, ["level", "health"]);
+// console.log(data);
+
+// const shape = {
+//     radius: 10,
+//     diameter() {
+//         return this.radius * 2;
+//     },
+//     perimeter: () => 2 * Math.PI * this.radius
+// }
+
+// console.log(shape.diameter())
+// console.log(shape.perimeter())
+
+let user = {
+  name: "Prithwish",
+  age: 22,
+  her: {
+    first: "Sneha",
+    last: "Mukherjee",
+  },
+};
+
+const name = "Sneha";
+
+const {
+  her: { first },
+} = user;
+
+console.log(first);
+
+function getItems(fruitList, favFruit, ...args) {
+  return [...fruitList, ...args, favFruit];
+}
+
+console.log(getItems(["Banana", "Apple"], "Pear", "Orange"));
+
+// [Banana, Apple, Pear, Orange]
+
+let c = { greeting: "hey!" };
+let d;
+
+d = c;
+
+c.greeting = "hello";
+console.log(d.greeting);
+
+console.log({ a: 1 } == { a: 1 });
+console.log({ a: 1  } === { a: 1 });
+
+let person = { name: "Lydia" };
+const members = [person];
+person = [null];
+
+console.log(members);
+
+const value = { number: 10}
+
